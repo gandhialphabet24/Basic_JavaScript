46752394cf08a54f5ac8b490acd23ce4586c30c3
gandhialphabet24 <gndhlphbt@gmail.com>
2024-08-07 13:15:07 +0700

    first commit

---
+++ Basic.js
@@ -0,0 +1,17 @@
+class persegi {
+  constructor(sisi) {
+    this.sisi = sisi;
+  }
+
+  luas = () => {
+    return this.sisi * this.sisi;
+  };
+
+  keliling = () => {
+    return 4 * this.sisi;
+  };
+}
+
+let kotak = new persegi(5);
+console.log("Luas : " + kotak.luas());
+console.log("Keliling : " + kotak.keliling());
